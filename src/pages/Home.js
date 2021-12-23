21e1ee23b31d5116fc90fb55022aaccf7c77f54a
aayushsrivastava9760 <81965095+aayushsrivastava9760@users.noreply.github.com>
2021-12-23 16:24:55 +0530

    added professions and cont at home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,55 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import professions from '../assets/professionData'
 
 const Home = () => {
+
+    const [index,setIndex] = useState(0)
+    const [subIndex,setSubIndex] = useState(0)
+    const [reverse,setReverse] = useState(false)
+
+    useEffect(()=>{
+        if(index === professions.length){
+          setReverse(false)
+          setIndex(0)
+          return 
+        }
+    
+        if( subIndex === professions[index].length + 1 && !reverse ){
+          setReverse(true)
+          
+          return
+        }
+    
+        if(subIndex === 0 && reverse){
+          setReverse(false)
+          if(index === professions.length -1){
+            setIndex(0)
+          }
+          else{
+            setIndex(prev => prev+1)
+          }
+          return
+        }
+    
+        const timeout = setTimeout(()=>{
+          setSubIndex(prev => prev + (reverse ? -1 : 1))
+        },100)
+    
+        return () => clearTimeout(timeout)
+    
+      },[index,subIndex,reverse])
+
+
   return (
     <div>
         <div className='color-block'/>
         <div className='home-col'>
             <img className='image-block' src="/aayush.jpeg" alt="aayush" />
             <div className='home-content'>
-                
+
                 <h1 className='home-title'>Hi,  I 'm   Aayush  Srivastava </h1>
-                {/* <h2 className='home-profession'>a { `${professions[index].substring(0, subIndex)}` }<span className='mark'>|</span></h2> */}
+                <h2 className='home-profession'>a { `${professions[index].substring(0, subIndex)}` }<span className='mark'>|</span></h2>
                 <p className='intro'>
                     I'm a student developer currently pursuing my Btech from IIT (BHU) , Varanasi . I am very passionate about software development, web development in particular . I am also into open source and love collaborating .
                 </p>
